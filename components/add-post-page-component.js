09e8959935d387a3c24f94c47f3d925841cab0eb
Anatoliy <akonst008@gmail.com>
2026-05-18 15:29:40 +0300

    add README and posting form

--- components/add-post-page-component.js
+++ components/add-post-page-component.js
@@ -1,13 +1,17 @@
 import { renderUploadImageComponent } from "./upload-image-component.js";
+import { renderHeaderComponent } from "./header-component.js";
 
 export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
   const render = () => {
     const appHtml = `
     <div class="page-container">
       <div class="header-container"></div>
-      <div class="upload-image-container"></div>
-      <input class="input" id="post_desc" placeholder="Описание">
-      <button class="button" id="add-button">Добавить</button>
+      <div class="add-post-form">
+        <p class="form-title">Добавить новый пост</p>
+        <div class="upload-image-container"></div>
+        <input class="input" id="post_desc" placeholder="Описание">
+        <button class="button" id="add-button">Добавить</button>
+      </div>
     </div>
   `;
 
@@ -23,6 +27,9 @@ export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
         },
       });
     }
+    renderHeaderComponent({
+      element: document.querySelector(".header-container"),
+    });
 
     document.getElementById("add-button").addEventListener("click", () => {
       onAddPostClick({
